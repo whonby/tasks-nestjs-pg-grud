494c3bd94f5a5588d6669584ea7e75a925497f94
guei217 <gueiroland1@gmail.com>
2019-08-20 14:06:58 +0000

    add findById and delete task

--- src/tasks/tasks.controller.ts
+++ src/tasks/tasks.controller.ts
@@ -1,6 +1,7 @@
-import {Body, Controller, Get, Post} from '@nestjs/common';
+import {Body, Controller, Delete, Get, Param, Post} from '@nestjs/common';
 import {TasksService} from "./tasks.service";
 import {Task} from "./task.model";
+import {CreateTaskDto} from "./dto/create-task.dto";
 
 @Controller('tasks')
 export class TasksController {
@@ -11,10 +12,18 @@ export class TasksController {
               return this.tasksService.getAllTasks();
             }
 
-        @Post()
-        createTask(@Body("title") title:string,
-                   @Body("description") description:string):Task{
-        return this.tasksService.createTask(title,description);
+       @Get('/:id')
+       getTaskById(@Param('id') id :string){
+        return this.tasksService.getTaskById(id);
+       }
+       @Post()
+        createTask(@Body() createTaskDto: CreateTaskDto):Task{
+        return this.tasksService.createTask(createTaskDto);
+        }
+
+        @Delete('/:id')
+    deleteTask(@Param('id') id:string):void{
+           this.tasksService.deleteTask(id);
         }
 
 }
